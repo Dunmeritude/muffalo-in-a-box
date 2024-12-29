d85de2f2a3d3c210f23927e4c9b8123ba7ab5f81
Jens <Jennalele@hotmail.com>
2024-12-28 17:49:00 -0800

    Initial Upload
    Pending bugfix merges

---
+++ 1.5/Source/DefOfs.cs
@@ -0,0 +1,14 @@
+﻿namespace StasisBox
+{
+#pragma warning disable CA2211 // Non-constant fields should not be visible
+	[DefOf]
+	public static class DefOfs
+	{
+		static DefOfs()
+		{
+			DefOfHelper.EnsureInitializedInCtor(typeof(DefOfs));
+		}
+
+		public static JobDef StasisBox_BoxPawn;
+	}
+}
---
+++ 1.5/Source/PawnKindGenderPair.cs
@@ -0,0 +1,9 @@
+﻿namespace StasisBox
+{
+#pragma warning disable CA1051
+	public class PawnKindGenderPair
+	{
+		public PawnKindDef pawnKindDef;
+		public Gender genderOfPawn;
+	}
+}
